8fca1f02fb3437cb641b6a121175e128aa7f2dcc
Richo <daininoricho@yahoo.com>
2022-12-21 15:50:27 +0700

    feat: profile page

---
+++ app/Views/pages/profile.php
@@ -0,0 +1,92 @@
+<section class="col-span-5 flex flex-col text-center items-center justify-center">
+    <section class="w-full relative bg-[url(https://images.unsplash.com/photo-1558979159-2b18a4070a87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80)] bg-cover bg-center bg-no-repeat">
+        <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25 dark:sm:from-gray-900/95 dark:sm:to-gray-900/25"></div>
+
+        <div class="relative px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+            <div class="text-center sm:text-left">
+                <h1 class="text-3xl font-extrabold sm:text-5xl dark:text-white mb-4">
+                    Mari bersama mencari
+
+                    <strong class="block font-extrabold text-blue-900 dark:text-blue-400 mt-2">
+                        Jati Diri.
+                    </strong>
+                </h1>
+
+                <p class="max-w-lg sm:text-xl sm:leading-normal dark:text-white">
+                    Klub Motor Jagoan Neon sudah bersama-sama sejak tahun 2019. Kami adalah klub motor yang berada di kota Malang, Jawa Timur.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Reservation -->
+    <section class="bg-white dark:bg-gray-800">
+        <div class="mx-auto max-w-screen-2xl px-4 pt-16 pb-12 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 lg:grid-cols-2">
+                <div class="relative z-10 lg:py-16">
+                    <div class="relative">
+                        <!-- <img alt="Bengkel" src="https://images.unsplash.com/photo-1578474005126-89909099fed6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" class="absolute inset-0 h-full w-full object-cover" /> -->
+
+                        <div id="default-carousel" class="relative" data-carousel="static">
+                            <!-- Carousel wrapper -->
+                            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                                <!-- Item 1 -->
+                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                    <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
+                                    <img src="https://images.unsplash.com/photo-1578474005126-89909099fed6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" class="absolute inset-0 h-full w-full object-cover" alt="...">
+                                </div>
+                                <!-- Item 2 -->
+                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                    <img src="https://images.unsplash.com/photo-1574288859684-d97c0fc8dcaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80" class="absolute inset-0 h-full w-full object-cover" alt="...">
+                                </div>
+                                <!-- Item 3 -->
+                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                    <img src="https://images.unsplash.com/photo-1558980664-3a031cf67ea8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="absolute inset-0 h-full w-full object-cover" alt="...">
+                                </div>
+                            </div>
+                            <!-- Slider indicators -->
+                            <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                            </div>
+                            <!-- Slider controls -->
+                            <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+                                    </svg>
+                                    <span class="sr-only">Previous</span>
+                                </span>
+                            </button>
+                            <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                                <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                                    </svg>
+                                    <span class="sr-only">Next</span>
+                                </span>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="relative flex items-center bg-gray-100 dark:bg-gray-900">
+                    <span class="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-gray-100 dark:lg:bg-gray-900"></span>
+
+                    <div class="py-8 pr-8 pl-4 sm:py-16 sm:pr-16 sm:pl-8 lg:py-24 lg:pr-24 lg:pl-12">
+                        <h2 class="text-2xl font-bold sm:text-3xl text-gray-900 dark:text-white">
+                            Pencapaian Kami
+                        </h2>
+
+                        <p class="mt-4 text-gray-600 font-medium dark:text-gray-100">
+                            Dari mendirikan sebuah bengkel komunitas bersama, mendapatkan penghargaan motor modifikasi terkeren, hingga menjadi komunitas motor terbesar di Malang.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+</section>
