18b326a325c3cb0d9e2013a668890da90c1c55fb
Richo <daininoricho@yahoo.com>
2022-12-21 23:35:03 +0700

    feat: artikel page

---
+++ app/Views/pages/artikel.php
@@ -0,0 +1,133 @@
+<section class="col-span-5 flex flex-col items-center my-12">
+    <!-- artikel 1 -->
+    <article class="flex bg-white transition hover:shadow-xl dark:bg-gray-700 w-3/4 h-52 mb-6">
+        <div class="rotate-180 p-2 [writing-mode:_vertical-lr]">
+            <time datetime="2022-10-10" class="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900 dark:text-white">
+                <span>2021</span>
+                <span class="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                <span>Dec 14</span>
+            </time>
+        </div>
+
+        <div class="hidden sm:block sm:basis-52">
+            <img alt="Thumbnail Artikel" src="https://images.unsplash.com/photo-1524591652733-73fa1ae7b5ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="aspect-square h-full object-cover" />
+        </div>
+
+        <div class="flex flex-1 flex-col justify-between">
+            <div class="border-l border-gray-900/10 dark:border-white/10 p-4 sm:border-l-transparent sm:p-6">
+                <a href="#">
+                    <h3 class="font-bold text-lg uppercase text-gray-900 dark:text-white">
+                        Memilih warna yang tepat buat motormu - 5 tips
+                    </h3>
+                </a>
+
+                <p class="mt-2 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Gaya motor udah oke. Style pengendara udah gaul. Gandengan, juga udah siap nih. Tapi kok warna motornya gak nyambung. Nah, kali ini kita akan membahas 5 tips memilih warna motor yang tepat. Tips 1,
+                </p>
+
+            </div>
+            <a href="#" class="block bg-blue-700 px-5 py-2.5 text-center font-bold uppercase text-white transition hover:bg-blue-800">
+                Baca Lebih Lanjut
+            </a>
+        </div>
+    </article>
+
+    <!-- artikel 2 -->
+    <article class="flex bg-white transition hover:shadow-xl dark:bg-gray-700 w-3/4 h-52 mb-6">
+        <div class="rotate-180 p-2 [writing-mode:_vertical-lr]">
+            <time datetime="2022-10-10" class="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900 dark:text-white">
+                <span>2022</span>
+                <span class="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                <span>Jan 31</span>
+            </time>
+        </div>
+
+        <div class="hidden sm:block sm:basis-52">
+            <img alt="Thumbnail Artikel" src="https://images.unsplash.com/photo-1565701964127-a3f37fdbb46b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1029&q=80" class="aspect-square h-full object-cover" />
+        </div>
+
+        <div class="flex flex-1 flex-col justify-between">
+            <div class="border-l border-gray-900/10 dark:border-white/10 p-4 sm:border-l-transparent sm:p-6">
+                <a href="#">
+                    <h3 class="font-bold text-lg uppercase text-gray-900 dark:text-white">
+                        Panduan Memilih Sepeda Motor Terbaik untuk Kebutuhan Anda
+                    </h3>
+                </a>
+
+                <p class="mt-2 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Saat memilih sepeda motor, penting untuk mempertimbangkan kebutuhan dan prioritas Anda. Apakah Anda membutuhkan sepeda
+                </p>
+
+            </div>
+            <a href="#" class="block bg-blue-700 px-5 py-2.5 text-center font-bold uppercase text-white transition hover:bg-blue-800">
+                Baca Lebih Lanjut
+            </a>
+        </div>
+    </article>
+
+    <!-- artikel 3 -->
+    <article class="flex bg-white transition hover:shadow-xl dark:bg-gray-700 w-3/4 h-52 mb-6">
+        <div class="rotate-180 p-2 [writing-mode:_vertical-lr]">
+            <time datetime="2022-10-10" class="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900 dark:text-white">
+                <span>2022</span>
+                <span class="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                <span>Feb 4</span>
+            </time>
+        </div>
+
+        <div class="hidden sm:block sm:basis-52">
+            <img alt="Thumbnail Artikel" src="https://images.unsplash.com/photo-1526956378276-b120acd89f17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" class="aspect-square h-full object-cover" />
+        </div>
+
+        <div class="flex flex-1 flex-col justify-between">
+            <div class="border-l border-gray-900/10 dark:border-white/10 p-4 sm:border-l-transparent sm:p-6">
+                <a href="#">
+                    <h3 class="font-bold text-lg uppercase text-gray-900 dark:text-white">
+                        5 Tips Aman Berkendara Motor
+                    </h3>
+                </a>
+
+                <p class="mt-2 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Mengendarai sepeda motor bisa menjadi moda transportasi yang menyenangkan dan nyaman, namun penting untuk selalu mengutamakan keselamatan. Berikut adalah lima tips berkendara sepeda motor yang
+                </p>
+
+            </div>
+            <a href="#" class="block bg-blue-700 px-5 py-2.5 text-center font-bold uppercase text-white transition hover:bg-blue-800">
+                Baca Lebih Lanjut
+            </a>
+        </div>
+    </article>
+
+    <!-- artikel 4 -->
+    <article class="flex bg-white transition hover:shadow-xl dark:bg-gray-700 w-3/4 h-52 mb-6">
+        <div class="rotate-180 p-2 [writing-mode:_vertical-lr]">
+            <time datetime="2022-10-10" class="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900 dark:text-white">
+                <span>2022</span>
+                <span class="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                <span>Feb 10</span>
+            </time>
+        </div>
+
+        <div class="hidden sm:block sm:basis-52">
+            <img alt="Thumbnail Artikel" src="https://images.unsplash.com/photo-1527965512280-9cf155bcb87d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="aspect-square h-full object-cover" />
+        </div>
+
+        <div class="flex flex-1 flex-col justify-between">
+            <div class="border-l border-gray-900/10 dark:border-white/10 p-4 sm:border-l-transparent sm:p-6">
+                <a href="#">
+                    <h3 class="font-bold text-lg uppercase text-gray-900 dark:text-white">
+                        Manfaat Mengendarai Sepeda Motor di Kota Ramai di Indonesia
+                    </h3>
+                </a>
+
+                <p class="mt-2 leading-relaxed text-gray-700 dark:text-gray-200">
+                    Mengendarai sepeda motor di kota-kota padat di Indonesia bisa menawarkan sejumlah keuntungan. Pertama, sepeda motor lebih gesit
+                </p>
+
+            </div>
+            <a href="#" class="block bg-blue-700 px-5 py-2.5 text-center font-bold uppercase text-white transition hover:bg-blue-800">
+                Baca Lebih Lanjut
+            </a>
+        </div>
+    </article>
+</section>
