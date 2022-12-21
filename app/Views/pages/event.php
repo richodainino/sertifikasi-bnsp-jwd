0901abb006d358ac80083aa562303a78a626f9cd
Richo <daininoricho@yahoo.com>
2022-12-22 00:22:55 +0700

    feat: event page

---
+++ app/Views/pages/event.php
@@ -0,0 +1,103 @@
+<section class="col-span-5 grid grid-cols-3 p-6">
+    <!-- event 1 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://images.unsplash.com/photo-1558980664-2cd663cf8dde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                20 Januari 2020
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    Neon Motor Club Ride
+                </h3>
+            </a>
+        </div>
+    </article>
+
+    <!-- event 2 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://images.unsplash.com/photo-1560199887-55dcf2cc769f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                23 Juni 2020
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    The Bali Motorcycle Tour
+                </h3>
+            </a>
+        </div>
+    </article>
+
+    <!-- event 3 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://plus.unsplash.com/premium_photo-1661884049104-9d79c968a64c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                27 Maret 2020
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    The Java Motorcycle Adventure
+                </h3>
+            </a>
+        </div>
+    </article>
+
+    <!-- event 4 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://images.unsplash.com/photo-1508147532797-432d81b6808f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                30 Januari 2021
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    The Sumatra Motorcycle Safari
+                </h3>
+            </a>
+        </div>
+    </article>
+
+    <!-- event 5 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://images.unsplash.com/photo-1591216105236-5ba45970702a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                14 Desember 2021
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    The Kalimantan Motorcycle Odyssey
+                </h3>
+            </a>
+        </div>
+    </article>
+
+    <!-- event 6 -->
+    <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-500/25 m-4 h-fit">
+        <img alt="Office" src="https://images.unsplash.com/photo-1575209426388-3f545c9a7490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80" class="h-48 w-full object-cover" />
+
+        <div class="bg-white p-4 dark:bg-gray-800 sm:p-6">
+            <time datetime="2022-10-10" class="block text-xs text-gray-500 dark:text-gray-400">
+                9 Februari 2022
+            </time>
+
+            <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900 dark:text-white">
+                    The Papua Motorcycle Expedition
+                </h3>
+            </a>
+        </div>
+    </article>
+</section>
