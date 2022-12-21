95b95fcc93faaff7619c4356f9081568b4b416a6
Richo <daininoricho@yahoo.com>
2022-12-21 16:16:04 +0700

    feat: visi misi view

---
+++ app/Views/pages/visimisi.php
@@ -0,0 +1,31 @@
+<section class="col-span-5 grid grid-cols-2 gap-4 py-16">
+    <details class="group [&_summary::-webkit-details-marker]:hidden pl-8">
+        <summary class="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-200 dark:bg-blue-900 text-gray-900 dark:text-white">
+            <h2 class="font-medium">Visi</h2>
+            <svg class="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+        </summary>
+
+        <p class="px-4 mt-4 leading-relaxed text-gray-700 dark:text-white">
+            Menjadikan Klub Motor Jagoan Neon sebagai Organisasi Motor yang bermartabat, solidaritas tinggi, tanpa kecelakaan, yang terkemuka dalam dunia otomotif.
+        </p>
+    </details>
+    <details class="group [&_summary::-webkit-details-marker]:hidden pr-8">
+        <summary class="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-200 dark:bg-blue-900 text-gray-900 dark:text-white">
+            <h2 class="font-medium">Misi</h2>
+            <svg class="ml-1.5 h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+        </summary>
+
+        <ol class="px-4 mt-4 list-decimal list-inside text-gray-700 dark:text-white">
+            <li>Menciptakan citra positif kepada masyarakat dalam setiap kegiatan</li>
+            <li>Berperan aktif dalam kegiatan otomotif khususnya Motor dan kegiatan sosial</li>
+            <li>Menjalin dan memelihara hubungan persaudaraan pada sesama anggota maupun komunitas lainnya</li>
+            <li>Berkomitmen mendahulukan keselamatan dan keamanan, serta tertib berlalulintas</li>
+            <li>Meningkatkan sumber daya organisasi secara berkelanjutan sebagai upaya meningkatkan apresiasi dan kemampuan para anggota dalam menghadapi kemajuan teknologi otomotif</li>
+            <li>Meningkatkan kualitas dan keahlian para anggota dalam berkendaraan untuk menjadi masyarakat otomotif yang berperilaku positif, baik dan benar sesuai dengan norma dan aturan yang berlaku</li>
+        </ol>
+    </details>
+</section>
