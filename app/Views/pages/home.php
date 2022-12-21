eb25c75c93b876b350e59bec5e2681a549b498a4
Richo <daininoricho@yahoo.com>
2022-12-21 15:50:18 +0700

    feat: home page

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,10 +1,10 @@
-<section class="col-span-5 bg-gray-50 dark:bg-gray-900 flex flex-col text-center justify-center">
-    <img src="https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image of motorcycle" class="opacity-30">
+<section class="col-span-5 flex flex-col text-center items-center justify-center">
+    <img src="https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image of motorcycle" class="opacity-50 dark:opacity-30 w-full">
     <div class="absolute">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world's potential</h1>
-        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-        <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-            Learn more
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Motor adalah Kehidupan</h1>
+        <p class="mb-6 text-lg font-medium text-gray-800 lg:text-xl sm:px-16 xl:px-48 dark:font-normal dark:text-gray-100">Selamat datang di website Klub Motor Jagoan Neon. Motto kami yakni motor adalah kehidupan. Ingin mencari tahu tentang kami lebih lanjut?</p>
+        <a href="/profile" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Klik Disini
             <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
