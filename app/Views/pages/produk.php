6fb5fb8c6a9cd001459399b80493a1875ab3394f
Richo <daininoricho@yahoo.com>
2022-12-21 17:47:44 +0700

    feat: product page

---
+++ app/Views/pages/produk.php
@@ -0,0 +1,83 @@
+<section class="col-span-5 text-center">
+    <div class="mx-auto max-w-screen-xl p-8">
+        <div class="grid grid-cols-6 gap-4 h-12">
+            <h2 class="text-2xl font-bold text-gray-900 md:text-3xl col-span-5 inline-flex items-center dark:text-white">
+                Jasa dan Produk yang Kami Sediakan
+            </h2>
+        </div>
+
+        <ul class="grid gap-4 mt-8 sm:grid-cols-1 lg:grid-cols-4 lg:grid-rows-2">
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1578474005126-89909099fed6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Servis Motor</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1570071677470-c04398af73ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Servis Mobil</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1574288859684-d97c0fc8dcaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Modifikasi Motor</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1649399337535-afbf61e74cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Part Motor</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1583912736562-6086dedde74b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Helmet</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1521337365556-4c5ff34dc056?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Jaket</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1558981396-5fcf84bdf14d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Ban</h5>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img class="rounded-t-lg h-52 w-full object-cover" src="https://images.unsplash.com/photo-1522850003946-16cb11d41a86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image of a product" />
+                    <div class="pt-5 pb-3">
+                        <h5 class="mb-2 text-lg font-normal tracking-tight text-brown dark:text-white">Speedometer</h5>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
