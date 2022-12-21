dc20f91a21ad1b69d1e26ffa0e4ef215caf64349
Richo <daininoricho@yahoo.com>
2022-12-21 18:57:46 +0700

    feat: about us page

---
+++ app/Views/pages/about.php
@@ -0,0 +1,65 @@
+<section class="col-span-5">
+    <section class="relative py-8 bg-blueGray-50 mt-8">
+        <div class="container mx-auto">
+            <div class="flex flex-wrap items-center justify-center">
+                <div class="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white w-full shadow-lg rounded-lg bg-blue-700">
+                        <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" class="w-full align-middle rounded-t-lg">
+                        <blockquote class="relative p-8 mb-4">
+                            <h4 class="text-xl font-bold text-white">
+                                Kilas balik Klub Motor Jagoan Neon
+                            </h4>
+                            <p class="text-md font-light mt-2 text-white">
+                                Didirikan pada tahun 2019, Putu Tomas dan Abud Neon adalah dua orang yang memiliki passion yang sama yaitu motor.
+                                Mereka pun memutuskan untuk mendirikan sebuah klub motor yang bernama Neon Motor Club.
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+
+                <div class="w-full md:w-6/12 px-4">
+                    <div class="flex flex-wrap">
+                        <div class="w-full md:w-6/12 px-4">
+                            <div class="relative flex flex-col mt-4">
+                                <div class="p-2 flex-auto">
+                                    <h6 class="text-xl mb-1 font-semibold dark:text-white">Event Pertama</h6>
+                                    <p class="mb-4 text-blueGray-500 dark:text-white">
+                                        Pada awal tahun 2020, Neon Motor Club mengadakan event pertama yaitu Neon Motor Club Ride. Event ini diadakan untuk mengenalkan klub kepada masyarakat luas.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="relative flex flex-col min-w-0">
+                                <div class="p-2 flex-auto">
+                                    <h6 class="text-xl mb-1 font-semibold dark:text-white">
+                                        Berpartisipasi dalam Perlombaan
+                                    </h6>
+                                    <p class="mb-4 text-blueGray-500 dark:text-white">
+                                        Di pertengahan tahun 2020, Neon Motor Club mengikuti lomba-lomba yang diadakan oleh beberapa klub motor di Bali.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full md:w-6/12 px-4">
+                            <div class="relative flex flex-col min-w-0 mt-4">
+                                <div class="p-2 flex-auto">
+                                    <h6 class="text-xl mb-1 font-semibold dark:text-white">Kemenangan Lomba Pertama</h6>
+                                    <p class="mb-4 text-blueGray-500 dark:text-white">
+                                        2021 adalah tahun yang beruntung bagi Neon Motor Club. Pada tahun ini, Neon Motor Club berhasil meraih juara pertama dalam perlombaan yang diadakan oleh klub motor lain.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="relative flex flex-col min-w-0">
+                                <div class="p-2 flex-auto">
+                                    <h6 class="text-xl mb-1 font-semibold dark:text-white">Perubahan Nama</h6>
+                                    <p class="mb-4 text-blueGray-500 dark:text-white">
+                                        Pada tanggal 23 Juni 2021, Neon Motor Club mengubah namanya menjadi Klub Motor Jagoan Neon.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
